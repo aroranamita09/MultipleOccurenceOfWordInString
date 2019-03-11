901dfcbdb249d5f72bb7b129bf72a015291e8f64
Namita <aroranamita33@gmail.com>
2019-03-11 10:19:58 +0530

    comp

--- test/TotalOccurencesTest.java
+++ test/TotalOccurencesTest.java
@@ -1,3 +1,4 @@
+package com.stackroute;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
@@ -9,7 +10,7 @@ public class TotalOccurencesTest {
     TotalOccurences obj;
     @Before
     public void setUp() throws Exception {
-        TotalOccurences obj = new TotalOccurences();
+         obj = new TotalOccurences();
     }
 
     @After
